63b23316b7e985617c48729109df7df4f15e9636
tfelde <timothyfelde@gmail.com>
2025-04-17 15:26:42 -0700

    Implemented Matches.java and MatchesDAO.java

--- app/src/main/java/com/cst338/cst338_project02_group3/database/MatchesDAO.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/database/MatchesDAO.java
@@ -1,4 +1,19 @@
 package com.cst338.cst338_project02_group3.database;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.cst338.cst338_project02_group3.database.entities.Matches;
+
+import java.util.List;
+
+@Dao
 public interface MatchesDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Matches match);
+
+    @Query("SELECT * FROM " + DatingAppDatabase.MATCHES_TABLE)
+    List<Matches> getAllRecords();
 }
