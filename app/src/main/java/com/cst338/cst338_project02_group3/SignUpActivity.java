527a58a42a7972680f4500e425c9e0e4975492e8
NeilCabanilla <ncabanilla@csumb.edu>
2025-04-17 13:59:38 -0700

    Added Intent factory for SignUpActivity and added a click listener to the button which shows a Toast, removed errors in the strings.xml, changed one thing in the activity_Login.xml in the constraint part

--- app/src/main/java/com/cst338/cst338_project02_group3/SignUpActivity.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/SignUpActivity.java
@@ -1,6 +1,10 @@
 package com.cst338.cst338_project02_group3;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +19,23 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
 
+
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignUpActivity.this,"Hooray! Successful creating an account", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    //This switches to different view (SignUpActivity)
+    //Takes in current context which is MainActivity
+    //Returns new intent having the MainActivity as the first param( The context) and switching to the view which is SignUpActivity.class)
+    static Intent signUpActivityIntentFactory(Context context) {
+        return new Intent(context, SignUpActivity.class);
+    }
+
+
+
 }
+
