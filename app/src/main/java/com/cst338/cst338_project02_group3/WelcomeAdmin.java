59e02180345c20da054cd38951e3d3e361b85e64
shancabi <shannyn.cabi2022@gmal.com>
2025-04-17 14:52:09 -0700

    -created logout_menu.xml -created logout methods in WelcomeAdmin -in progress

--- app/src/main/java/com/cst338/cst338_project02_group3/WelcomeAdmin.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/WelcomeAdmin.java
@@ -1,14 +1,28 @@
 package com.cst338.cst338_project02_group3;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomeAdmin extends AppCompatActivity {
+    private static final String MAIN_ACTIVITY_USER_ID = "com.cst338.cst338_project02_group3.MAIN_ACTIVITY_USER_ID";
+    private static final int LOGGED_OUT = -1;
+    static final String SHARED_PREFERENCE_USERID_KEY = "com.cst338.cst338_project02_group3.SHARED_PREFERENCE_USERID_KEY";
+    static final String SHARED_PREFERENCE_USERID_VALUE = "com.cst338.cst338_project02_group3.SHARED_PREFERENCE_USERID_VALUE";
+    private static final String SAVED_INSTANCE_STATE_USERID_KEY = "com.cst338.cst338_project02_group3.SAVED_INSTANCE_STATE_USERID_KEY";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,61 @@ public class WelcomeAdmin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.logoutMenuItem);
+        item.setVisible(true);
+        if(user == null){
+            return false;
+        }
+        item.setTitle(user.getUsername());
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                showLogoutDialog();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void showLogoutDialog(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(WelcomeAdmin.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        alertBuilder.setMessage("Logout?");
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
+    }
+
+    private void logout() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(SHARED_PREFERENCE_USERID_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+        sharedPrefEditor.putInt(SHARED_PREFERENCE_USERID_KEY,LOGGED_OUT);
+        sharedPrefEditor.apply();
+        getIntent().putExtra(MAIN_ACTIVITY_USER_ID,LOGGED_OUT);
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
     }
 }
