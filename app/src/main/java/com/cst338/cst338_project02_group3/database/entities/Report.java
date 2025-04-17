0998423b653f4fba5100ff3086f55185e5a6e6e8
NeilCabanilla <ncabanilla@csumb.edu>
2025-04-17 15:46:47 -0700

    Added a new variable in User entity called isBan, created and started working on Report entity as well as ReportDAO, but might have to tweak it later on...

---
+++ app/src/main/java/com/cst338/cst338_project02_group3/database/entities/Report.java
@@ -0,0 +1,69 @@
+package com.cst338.cst338_project02_group3.database.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.cst338.cst338_project02_group3.database.DatingAppDatabase;
+
+import java.util.Objects;
+
+@Entity(tableName = DatingAppDatabase.REPORT_TABLE)
+public class Report {
+    //TODO: Might have to double check all these things
+    @PrimaryKey(autoGenerate = true)
+    private int reportId;
+    private int userId;
+    private String reason;
+    private boolean isBan;
+
+    public Report(int reportId, int userId, String reason, boolean isBan) {
+        this.reportId = reportId;
+        this.userId = userId;
+        this.reason = reason;
+        this.isBan = isBan;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return reportId == report.reportId && userId == report.userId && isBan == report.isBan && Objects.equals(reason, report.reason);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reportId, userId, reason, isBan);
+    }
+
+    public boolean isBan() {
+        return isBan;
+    }
+
+    public void setBan(boolean ban) {
+        isBan = ban;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public int getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(int reportId) {
+        this.reportId = reportId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
--- app/src/main/java/com/cst338/cst338_project02_group3/database/entities/User.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/database/entities/User.java
@@ -16,23 +16,25 @@ public class User {
     private String username;
     private String password;
     private boolean isAdmin;
+    private boolean isBan;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
         isAdmin = false;
+        isBan = false;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+        return id == user.id && isAdmin == user.isAdmin && isBan == user.isBan && Objects.equals(username, user.username) && Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password, isAdmin);
+        return Objects.hash(id, username, password, isAdmin, isBan);
     }
 
     public int getId() {
@@ -65,5 +67,13 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public boolean isBan() {
+        return isBan;
+    }
+
+    public void setBan(boolean ban) {
+        isBan = ban;
     }
 }
