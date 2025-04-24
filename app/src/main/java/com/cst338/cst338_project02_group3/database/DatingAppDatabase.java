73b41c3c8544b109bf03c4e043dea6e65a5fc18d
Daniel Everman <iced8383@gmail.com>
2025-04-23 18:37:42 -0700

    Fixed AndroidManifest.xml and the database

--- app/src/main/java/com/cst338/cst338_project02_group3/database/DatingAppDatabase.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/database/DatingAppDatabase.java
@@ -19,7 +19,7 @@ import com.cst338.cst338_project02_group3.database.entities.UserPreferences;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {User.class, UserInfo.class, Matches.class, Report.class, UserPreferences.class}, version = 9, exportSchema = false)
+@Database(entities = {User.class, UserInfo.class, Matches.class, Report.class, UserPreferences.class}, version = 10, exportSchema = false)
 public abstract class DatingAppDatabase extends RoomDatabase {
     public static final String DATABASE_NAME = "DatingAppDatabase";
     public static final String USER_TABLE = "userTable";
@@ -67,7 +67,7 @@ public abstract class DatingAppDatabase extends RoomDatabase {
 
                 User testUser1 = new User("testuser1", "testuser1");
 
-                dao.insert(testUser1);
+                userDAO.insert(testUser1);
 
 
                 //Report User test
