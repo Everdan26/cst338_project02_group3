2bf1b84eeecd70f48d4638fbea8987afd1ec2e45
NeilCabanilla <ncabanilla@csumb.edu>
2025-04-30 15:30:06 -0700

    Moved all the work of FindMatchesResult to FindMatches.xml file

--- app/src/main/java/com/cst338/cst338_project02_group3/FindMatchesResult.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/FindMatchesResult.java
@@ -8,17 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.cst338.cst338_project02_group3.database.entities.UserInfo;
 import com.cst338.cst338_project02_group3.databinding.ActivityFindMatchesResultBinding;
 import com.cst338.cst338_project02_group3.databinding.ActivityReportLogsBinding;
 
 public class FindMatchesResult extends AppCompatActivity {
-    ActivityFindMatchesResultBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityFindMatchesResultBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
+        //To be Deleted
     }
 }
