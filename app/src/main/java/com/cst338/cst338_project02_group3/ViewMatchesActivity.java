4279b2cad496b1b4b65e6977bb5b2122c272ed88
shancabi <shannyn.cabi2022@gmal.com>
2025-04-29 22:58:36 -0700

    -fixed back button on my matches

--- app/src/main/java/com/cst338/cst338_project02_group3/ViewMatchesActivity.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/ViewMatchesActivity.java
@@ -33,36 +33,38 @@ public class ViewMatchesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityViewMatchesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+//
+//        repository = DatingAppRepository.getRepository(getApplication());
+//
+//        loggedInUserId = getIntent().getIntExtra(USER_MATCHES_ACTIVITY_USER_ID, -1);
+//        if (loggedInUserId != -1) {
+//
+//            repository.getUserInfoByUserId(Matches.getUserId1()).observe(this, userInfo -> {
+//                if (userInfo != null) {
+//                    binding.textViewName.setText(userInfo.getName());
+//                    binding.textViewAge.setText("Age: " + userInfo.getAge());
+//                    binding.textViewBio.setText("Bio: " + userInfo.getBio());
+//
+//                    Glide.with(binding.profileImg.getContext())
+//                            .load(userInfo.getPhoto())
+//                            .into(binding.profileImg);
+//                }
+//            });
+//        }
 
-        repository = DatingAppRepository.getRepository(getApplication());
-
-        loggedInUserId = getIntent().getIntExtra(USER_MATCHES_ACTIVITY_USER_ID, -1);
-        if (loggedInUserId != -1) {
-            Matches match = matches.get(0);
-
-            repository.getUserInfoByUserId(match.getUserId1()).observe(this, userInfo -> {
-                if (userInfo != null) {
-                    binding.textViewName.setText(userInfo.getName());
-                    binding.textViewAge.setText("Age: " + userInfo.getAge());
-                    binding.textViewBio.setText("Bio: " + userInfo.getBio());
-
-                    Glide.with(binding.profileImg.getContext())
-                            .load(userInfo.getPhoto())
-                            .into(binding.profileImg);
-                }
-            });
-        }
         //TODO: add back button on matches page
-        //binding.viewMatchesBackButton.setOnClickListener(v -> {
-        //    onBackPressed();
-        //});
-    //}
+        binding.viewMatchesBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = WelcomeUser.welcomeUserIntentFactory(getApplicationContext(),loggedInUserId);
+                startActivity(intent);
+            }
+        });
+    }
 
 
     static Intent viewMatchesIntentFactory(Context context) {
             Intent intent = new Intent(context, ViewMatchesActivity.class);
-            intent.putExtra(USER_MATCHES_ACTIVITY_USER_ID, userId);
             return intent;
         }
     }
-}
