55e3cfdfed28b99fa9c47345ef8d44235dc11735
Daniel Everman <daniel@everman.info>
2025-04-30 18:08:03 -0700

    Added welcomeUserIntentTest

--- app/src/androidTest/java/com/cst338/cst338_project02_group3/IntentTest.java
+++ app/src/androidTest/java/com/cst338/cst338_project02_group3/IntentTest.java
@@ -12,10 +12,10 @@ import org.junit.Test;
 public class IntentTest {
 
     private static final int TEST_VALUE = 1;
-    /**
-     * Keys into intent extras. Add them as you add tests.
-     */
+
+    //Intent keys. Keep adding them as you add intent tests.
     private static final String USER_PROFILE_ACTIVITY_USER_ID = "com.cst338.project02_group3.USER_PROFILE_ACTIVITY_USER_ID";
+    private static final String WELCOME_ACTIVITY_USER_ID = "com.cst338.project02_group3.WELCOME_ACTIVITY_USER_ID";
 
     @Test
     public void userProfileIntentTest() {
@@ -26,5 +26,12 @@ public class IntentTest {
         assertEquals(TEST_VALUE, intent.getIntExtra(USER_PROFILE_ACTIVITY_USER_ID, -1));
     }
 
+    @Test
+    public void welcomeUserIntentTest() {
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent intent = new Intent(context, WelcomeUser.class);
+        intent.putExtra(WELCOME_ACTIVITY_USER_ID, TEST_VALUE);
 
+        assertEquals(TEST_VALUE, intent.getIntExtra(WELCOME_ACTIVITY_USER_ID, -1));
+    }
 }
