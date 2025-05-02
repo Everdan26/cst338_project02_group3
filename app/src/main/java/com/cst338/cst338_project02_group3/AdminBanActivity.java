d84fe1b3a15335c9c13098e26552daf9716892a7
shancabi <shannyn.cabi2022@gmal.com>
2025-05-01 20:18:02 -0700

    -added main menu btn function on admin ban page

--- app/src/main/java/com/cst338/cst338_project02_group3/AdminBanActivity.java
+++ app/src/main/java/com/cst338/cst338_project02_group3/AdminBanActivity.java
@@ -3,6 +3,8 @@ package com.cst338.cst338_project02_group3;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.cst338.cst338_project02_group3.database.DatingAppRepository;
+import com.cst338.cst338_project02_group3.databinding.ActivityAdminBanBinding;
+
 public class AdminBanActivity extends AppCompatActivity {
+    private ActivityAdminBanBinding binding;
+    private int loggedInUserId;
+    private DatingAppRepository repository;
     private static final String USER_ADMIN = "com.cst338.project02_group3.USER_ADMIN";
 
     public static Intent adminBanActivityIntentFactory(Context context, int userId) {
@@ -22,12 +30,29 @@ public class AdminBanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_ban);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityAdminBanBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        loggedInUserId = getIntent().getIntExtra(USER_ADMIN, -1);
+
+        repository = DatingAppRepository.getRepository(getApplication());
+
+        binding.banLogsDisplay.setMovementMethod(new ScrollingMovementMethod());
+
+        //bannedUsers();
+
+        //main menu
+        binding.mainMenuAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = WelcomeUser.welcomeUserIntentFactory(getApplicationContext(),loggedInUserId);
+                startActivity(intent);
+            }
         });
-    }
+
+        }
+
+//    private void bannedUsers() {
+//        ArrayList<Ban>
+//    });
+//    }
 }
